d6ae5ee956ce384a56d6dfe19a7930370a480915
JoaopbPedro <joaopedrobertolo@hotmail.com>
2018-04-19 17:01:02 +0100

    atualizar

--- modeloform.php
+++ modeloform.php
@@ -1,74 +1,116 @@
 <?php
     
+
+
     if(isset($_POST['name']) && isset($_POST['password'])){
         $name = $_POST['name'];
         $pass = $_POST['password'];
+        
+        
+        ####CREATE USERS####
         $user = new User("Joao","3","123");
-        $array_workers = array("".$user->getName(),"".$user->getNivel(),"".$user->getPassword());
+        $array_nome = array("".$user->getName());$array_password = array("".$user->getPassword());$array_nivel = array("".$user->getNivel());
+        $user = new User("JP","1","123");
+        $array_nome = array("".$user->getName());$array_password = array("".$user->getPassword());$array_nivel = array("".$user->getNivel());
         $user = new User("Alvaro","2","123");
-        $array_director = array("".$user->getName(),"".$user->getNivel(),"".$user->getPassword());
+        array_push($array_nome,"".$user->getName());array_push($array_nivel,"".$user->getNivel());array_push($array_password,"".$user->getPassword());
         $user = new User("Mario","1","123");
-        $array_admin = array("".$user->getName(),"".$user->getNivel(),"".$user->getPassword());
-
+        array_push($array_nome,"".$user->getName());array_push($array_nivel,"".$user->getNivel());array_push($array_password,"".$user->getPassword());
+        
+        ####VERIFIYNG USER####
     
-        if($name == $array_workers[0]){
-            if($pass == $array_workers[2]){
-                Workers($name,$array_workers);
+        $find = false;
+        $position = 0;
+
+        for($i=0; $i<sizeof($array_nome);$i++){
+            if($name == $array_nome[$i]){
+                for($cont=0; $cont<sizeof($array_password);$cont++){
+                    if($pass == $array_password[$cont]){
+                        $find = true;
+                        $position = $i;
+                        $i = (sizeof($array_nome[$i]))+1;
+                        $cont = (sizeof($array_password[$cont]))+1;
+
+                    }
+                    else{
+                        $find = false;
+                    }
+                }
             }
             else{
-                echo "Nome de Utilizador e Password incorretos.!";
+                $find = false;
             }
         }
-        else if($name == $array_director[0]){
-            if($pass == $array_director[2]){
-                Directors();
-            }
-            else{
-                echo "Nome de Utilizador e Password incorretos.!";
-            }
-        }
-        else if($name == $array_admin[0]){
-            if($pass == $array_admin[2]){
-                Admins();
-            }
-            else{
-                echo "Nome de Utilizador e Password incorretos.!";
-            }
+
+        if($find == false){
+            echo "<div class='container' style='color:red;'>Nome de Utilizador e Password incorretos!</div>";
         }
         else{
-            echo "Nome de Utilizador e Password incorretos.!";
-        }
+            ####CREATE TABLE####
+            ?>
+            <div class="container">
+                <h3>Bem Vindo <?php echo $name; ?></h3>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Nivel</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    
+                        <?php
+                            if($array_nivel[$position] == "1"){
+                                $nivel = 0;
+                                while(sizeof($array_nivel)>$nivel){
+                                    if($array_nivel[$nivel] == "1"){
+                                        ?>
+                                            <tr>
+                                                <td><?php echo $array_nome[$nivel]; ?></td>
+                                                <td>1</td>
+                                            </tr>
+                                        <?php
+                                    }
 
-
-
-
-        
-    }
-
-    function Workers($name,$array_workers){
-        ?>
-            <h3>Bem Vindo <?php echo $name; ?></h3>
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Nivel</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td><?php ?></td>
-                    <td>Doe</td>
-                    <td>john@example.com</td>
-                </tr>
-                </tbody>
-            </table>
+                                    $nivel++;
+                                    
+                                }
+                            }
+                            else if($array_nivel[$position] == "2"){
+                                $nivel = 0;
+                                while(sizeof($array_nivel)>$nivel){
+                                    if($array_nivel[$nivel] == "2" || $array_nivel[$nivel] == "1"){
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $array_nome[$nivel]; ?></td>
+                                            <td><?php echo $array_nivel[$nivel]; ?></td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    $nivel++;
+                                }
+                            }
+                            else{
+                                $nivel = 0;
+                                while(sizeof($array_nivel)>$nivel){
+                                    
+                                        ?>
+                                            <tr>
+                                            <td><?php echo $array_nome[$nivel]; ?></td>
+                                            <td><?php echo $array_nivel[$nivel]; ?></td>
+                                        </tr>
+                                        <?php
+                                    $nivel++;
+                                }
+                            }
+                        ?>
+                        
+                    
+                    </tbody>
+                </table>
+            </div>
         <?php
+        }        
     }
-    function Directors(){
 
-    }
-    function Admins(){
-
-    }
 ?>
--- vistaform.php
+++ vistaform.php
@@ -7,13 +7,19 @@
     <title>Ficha7</title>
 </head>
 <body>
-    <form name="form" method="post">
-        <p>Name:</p>
-		 <INPUT class="form-control" TYPE = "text" name="name">
-		 <p>Password:</p>
-		 <INPUT class="form-control" TYPE = "password" name="password">
-		 <br>
-		 <INPUT class="btn btn-default" TYPE = "Submit" Name = "Submit1" VALUE = "Submit">
-    </form>
+
+    <div class="container">
+    <br><br>
+        <form name="form" method="post">
+            <p>Name:</p>
+            <INPUT class="form-control" TYPE = "text" name="name">
+            <p>Password:</p>
+            <INPUT class="form-control" TYPE = "password" name="password">
+            <br>
+            <INPUT class="btn btn-default" TYPE = "Submit" Name = "Submit1" VALUE = "Submit">
+        </form>
+        <br>
+    </div>
+    
 </body>
 </html>
